dabfbef732012eca47e709070f8814b8123a2f3a
qaedd <edd-alexander@hotmail.com>
2022-03-25 13:17:45 +0000

    updated docker

--- mobs/application/routes.py
+++ mobs/application/routes.py
@@ -1,2 +1,22 @@
 from application import app
 import requests
+from flask import request, jsonify
+from random import choice
+
+mobs = ['Dragon','Goblins','Owlbear','Fire Giant']
+
+@app.route('/mobs', methods=['POST'])
+def get-mob():
+    request_ = requests.json()
+    if request_ == "Volcano":
+        mobs.append('Fire Giant')
+        mobs.append('Fire Giant')
+    elif request_ == "Forest"
+        mobs.append('Owlbear')
+        mobs.append('Owlbear')
+    elif request_ == "On the road"
+        mobs.append('Goblin')
+        mobs.append('Goblin')
+    elif request_ == 'Tavern'
+        mobs.append('Dragon')
+        mobs.append('Dragon')
