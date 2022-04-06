cdb13fd530a9f4c0ae7e81f42d0aa240cc6b5e11
qaedd <edd-alexander@hotmail.com>
2022-04-06 14:27:42 +0000

    l

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -7,5 +7,5 @@ def index():
     location_ = requests.get('http://location:5000/get-location')
     weather_ = requests.get('http://weather:5000/get-weather')
     mobs_ = requests.post('http://mobs:5000/get-mobs', json=location_.json())
-    results = "you will be fighting " + mobs_.json()['mob'] + " by a " + location_.json()['location'] + "while the weather is " + weather_.json()['weat'] 
-    return render_template('index.html', results = results)
+    # results = "you will be fighting " + mobs_.json()['mob'] + " by a " + location_.json()['location'] + "while the weather is " + weather_.json()['weat'] 
+    return render_template('index.html', mob=mobs_.json(), location=location_.json(), weather=weather_.json())
