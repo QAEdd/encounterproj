b89d26aacd4474d78b244f0db516cc7b4f189fe7
qaedd <edd-alexander@hotmail.com>
2022-04-06 11:50:05 +0000

    l

--- front-end/application/routes.py
+++ front-end/application/routes.py
@@ -6,6 +6,6 @@ from flask import render_template, url_for
 def index():
     location_ = requests.get('http://location:5000/get-location')
     weather_ = requests.get('http://weather:5000/get-weather')
-    mobs_ = requests.post('http://mobs:5000/get-mobs', json=location_.json())
-    results = "you will be fighting " + mobs_.json + " by a " + location_.json() + "while the weather is " + weather_.json() 
+    mobs_ = requests.post('http://mobs:5000/get-mobs', json=location_.json()['location'])
+    results = "you will be fighting " + mobs_.json() + " by a " + location_.json() + "while the weather is " + weather_.json() 
     return render_template('index.html', results = results)
