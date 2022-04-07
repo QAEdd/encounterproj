d32f170cf6df819a98c6eed5e9eeb06ad84ad921
qaedd <edd-alexander@hotmail.com>
2022-04-07 09:14:05 +0000

    .

--- location/application/routes.py
+++ location/application/routes.py
@@ -3,10 +3,9 @@ import requests
 from flask import jsonify
 from random import choice
 
-locations = ["Forest","On the road","Fields","Volcano","Tavern","Cemetery","The void","just over there"]
+locations = ["Forest","On the road","Volcano","Tavern","Cemetery",]
 
 @app.route('/get-location', methods=['GET'])
 def get_location():
     location = choice(locations)
     return jsonify(location=location)
-    
