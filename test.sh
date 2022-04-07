90a7380fb6072296739a8382ef06a2cf3c953d05
qaedd <edd-alexander@hotmail.com>
2022-04-07 13:03:46 +0000

    l

--- test.sh
+++ test.sh
@@ -1,16 +1,15 @@
 
 #!/bin/bash
-declare -a directories=("front-end" "mobs" "weather" "location")
+declare -a directories=("tests" "tests" "weather" "location")
+sudo apt-get update
+sudo apt-get install python3 python3-pip python3-venv
+source venv/bin/activate
 pip3 install -r test_requirements.txt
+python3 -m venv venv
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  sudo apt-get update
-  sudo apt-get install python3 python3-pip python3-venv
-  python3 -m venv venv
-  source venv/bin/activate
   python3 -m pytest --cov=application --cov-report=html
   pwd
-  deactivate
   cd ..
 done
