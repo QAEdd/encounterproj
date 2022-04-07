889c9950200d944438ee01903a4f72960c84447f
qaedd <edd-alexander@hotmail.com>
2022-04-07 09:22:21 +0000

    Spelling improved

--- mobs/application/routes.py
+++ mobs/application/routes.py
@@ -3,7 +3,7 @@ import requests
 from flask import request, jsonify
 from random import choice
 
-mobs = ['Dragon','Goblins','Owlbear','Fire Giant']
+mobs = ['Dragon','Goblin Horde','Owlbear','Fire Giant']
 
 @app.route('/get-mobs', methods=['POST'])
 def get_mob():
@@ -12,13 +12,21 @@ def get_mob():
     if request_ == "Volcano":
         mobs.append('Fire Giant')
         mobs.append('Fire Giant')
+        mobs.append('Fire Giant')
+        mobs.append('Fire Giant')
     elif request_ == "Forest":
         mobs.append('Owlbear')
         mobs.append('Owlbear')
+        mobs.append('Owlbear')
+        mobs.append('Owlbear')
     elif request_ == "On the road":
-        mobs.append('Goblin')
-        mobs.append('Goblin')
+        mobs.append('Goblin Horde')
+        mobs.append('Goblin Horde')
+        mobs.append('Goblin Horde')
+        mobs.append('Goblin Horde')
     elif request_ == 'Tavern':
+        mobs.append('Dragon')
+        mobs.append('Dragon')
         mobs.append('Dragon')
         mobs.append('Dragon')
     mobb = choice(mobs)
