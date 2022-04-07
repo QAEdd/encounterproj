89c4ede7bf58fb2c8da3bf17ede7cee620e8efc1
qaedd <edd-alexander@hotmail.com>
2022-04-07 12:56:14 +0000

    j

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 declare -a directories=("front-end" "mobs" "weather" "location")
+pip3 install -r test_requirements.txt
 for dir in "${directories[@]}"
 do
   cd ${dir}
@@ -8,7 +9,6 @@ do
   sudo apt-get install python3 python3-pip python3-venv
   python3 -m venv venv
   source venv/bin/activate
-  pip3 install -r test_requirements.txt
   python3 -m pytest --cov=application --cov-report=html
   pwd
   deactivate
